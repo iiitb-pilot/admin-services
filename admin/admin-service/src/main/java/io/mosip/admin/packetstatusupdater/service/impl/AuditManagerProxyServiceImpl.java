5d13ac83c0b2faef1bb128ec72c74be087ab60e1
Vishal05-dev <vishalvenugopal00@gmail.com>
2026-07-16 14:37:06 +0530

    changes done for xml issue - Lost rid

--- admin/admin-service/src/main/java/io/mosip/admin/packetstatusupdater/service/impl/AuditManagerProxyServiceImpl.java
+++ admin/admin-service/src/main/java/io/mosip/admin/packetstatusupdater/service/impl/AuditManagerProxyServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -117,6 +118,8 @@ public class AuditManagerProxyServiceImpl implements AuditManagerProxyService {
 		request.setRequesttime(LocalDateTime.now());
 		request.setRequest(auditManagerRequestDto);
 
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<RequestWrapper<AuditManagerRequestDto>> entity = new HttpEntity<>(request);
 
 		try {
